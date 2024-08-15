d4fd196074d8e769ccabedcbbac6a3125dd9b80d
mayiwen <mayiwen@mayiwendeMacBook-Pro.local>
2024-08-15 23:25:42 +0800

    test

--- douyin_test.go
+++ douyin_test.go
@@ -1,23 +1,33 @@
 package douyinlive
 
 import (
-	"github.com/jwwsjlm/douyinLive/generated/douyin"
-	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
+	"DouyinLive/generated/douyin"
 	"log"
 	"testing"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 func TestNewDouyinLive(t *testing.T) {
-	d, _ := NewDouyinLive("644826113301")
+	d, _ := NewDouyinLive("96306655034")
 	d.Subscribe(func(eventData *douyin.Message) {
-		if eventData.Method == WebcastChatMessage {
-			msg := &douyin.ChatMessage{}
+		// if eventData.Method == WebcastChatMessage {
+		// 	msg := &douyin.ChatMessage{}
+
+		// 	proto.Unmarshal(eventData.Payload, msg)
+		// 	marshal, _ := protojson.Marshal(msg)
+		// 	//msg.String()
+		// 	log.Println("聊天msg", msg.User.Id, msg.User.NickName, msg.Content, string(marshal))
+		// }
+
+		if eventData.Method == WebcastGiftMessage {
+			msg := &douyin.GiftMessage{}
 
 			proto.Unmarshal(eventData.Payload, msg)
 			marshal, _ := protojson.Marshal(msg)
 			//msg.String()
-			log.Println("聊天msg", msg.User.Id, msg.User.NickName, msg.Content, string(marshal))
+			log.Println("礼物msg", msg.User.Id, msg.User.NickName, string(marshal))
 		}
 		//log.Println(eventData.Method, string(eventData.Payload))
 		//msg := &douyin.ChatMessage{}
